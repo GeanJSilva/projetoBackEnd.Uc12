074f0c75ef32124352c861f6b806c0a2270830be
Gean J. Silva <109292751+GeanJSilva@users.noreply.github.com>
2023-02-17 10:57:27 -0300

    Aula validar cnpj

--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ProjetoBackEnd.Interfaces;
 
@@ -25,6 +26,32 @@ namespace ProjetoBackEnd.Classes
         }
 
         public bool ValidarCnpj(string cnpj)
+        {
+            //Regex comparara se o valor informado segue o padrao descrito:
+            bool retornoCnpjValido =  Regex.IsMatch(cnpj, @"^(\d{14})|(\d{2}.\d{3}.\d{3}/\d{4}-\d{2}) $");
+
+            if (retornoCnpjValido)
+            { 
+                //faz uma checagem de partes especificas do valor:
+                string substringCnpj14 = cnpj.Substring(8, 4);
+
+                if (substringCnpj14 == "0001")
+                    {return true;}
+                  
+            }
+            else if (retornoCnpjValido)
+            {
+                string substringCnpj18 = cnpj.Substring(11, 4);
+
+                if (substringCnpj18 == "0001")
+                    {return true;}
+            }         
+
+           return false;
+
+        }
+
+        internal object ValidarCnpj()
         {
             throw new NotImplementedException();
         }
--- Program.cs
+++ Program.cs
@@ -1,61 +1,92 @@
 ﻿using ProjetoBackEnd.Classes;
 
-
-PessoaJuridica novaPJ = new PessoaJuridica();
-novaPJ.Nome = "VelhaAnglo";
-novaPJ.cnpj = "1234123412341324";
-novaPJ.rendimentos = 8000;
-
+//**********PESSOA FISICA***************
 
 PessoaFisica novaPf = new PessoaFisica();
-novaPf.Nome = "Gean";
-novaPf.cpf ="123456789";
-novaPf.rendimentos = 5000;
-novaPf.DataNasc = new DateTime(2010, 01, 01);
-//atribuir endereço:
 Endereco novoEndPf = new Endereco();
+// novaPf.Nome = "Gean";
+// novaPf.cpf ="123456789";
+// novaPf.rendimentos = 5000;
+// novaPf.DataNasc = new DateTime(2010, 01, 01);
 
-novoEndPf.logradouro = "Rua D";
-novoEndPf.numero = 100;
-novoEndPf.cep = 57000000;
-novoEndPf.complemento = "Curso Senai";
-novoEndPf.endComercial = true;
+// novoEndPf.logradouro = "Rua D";
+// novoEndPf.numero = 100;
+// novoEndPf.cep = 57000000;
+// novoEndPf.complemento = "Curso Senai";
+// novoEndPf.endComercial = true;
 
-novaPf.Endereco = novoEndPf;
+// novaPf.Endereco = novoEndPf;
 
-// Console.WriteLine(novaPf.Nome);
-// Console.WriteLine(novaPf.cpf);
-
-// //Interpolacao $:
-// Console.WriteLine($"Bem vindo, {novaPf.Nome} de cpf {novaPf.cpf}");
-
-// //Concatenação:
-// Console.WriteLine("Bem vindo, "+ novaPf.Nome);
+// // Console.WriteLine(novaPf.Nome);
+// // Console.WriteLine(novaPf.cpf);
 
 
-//Aula calcular imposto:
-    //pessoa fisica:
-    float ImpostoPagarPF = novaPf.CalcularImposto(novaPf.rendimentos);
-    Console.WriteLine(ImpostoPagarPF.ToString("C"));
+//**********PESSOA JURIDICA***************
+
+PessoaJuridica novaPJ = new PessoaJuridica();
 
 
-    //pessoa juridica
-        float ImpostoPagarPJ = novaPJ.CalcularImposto(novaPJ.rendimentos);
-        Console.WriteLine(ImpostoPagarPJ);
+novaPJ.Nome = "VelhaAnglo";
+novaPJ.rendimentos = 8000;
+novaPJ.cnpj = "59184301200115";
+Endereco novoEndPJ = new Endereco();
+novaPJ.Endereco = novoEndPJ;
 
-    //
+novoEndPJ.logradouro = "Rua D";
+novoEndPJ.numero = 100;
+novoEndPJ.cep = 57000000;
+novoEndPJ.complemento = "Curso Senai";
+novoEndPJ.endComercial = true;
 
-    //validar data de nascimento:
-        Console.WriteLine(novaPf.ValidarDataNasc("01/jan/2000"));
-    //
+//Console.WriteLine(novaPJ.ValidarCnpj("59184301000115"));
+//Console.WriteLine(novaPJ.ValidarCnpj("97.310.609/0001-07"));
 
-    //mostrar o endereço total:
-        //@ formatar o formulario como quiser: 
-        Console.WriteLine(@$"
+
+//Interpolacao $:
+Console.WriteLine(@$"
+Nome: {novaPJ.Nome} 
+Endereço: {novoEndPJ.logradouro}, Num: {novoEndPJ.numero}
+CNPJ: {novaPJ.cnpj} - Valido: {novaPJ.ValidarCnpj(novaPJ.cnpj)}
+");
+
+
+
+
+
+
+// // //Interpolacao $:
+// // Console.WriteLine($"Bem vindo, {novaPf.Nome} de cpf {novaPf.cpf}");
+
+// // //Concatenação:
+// // Console.WriteLine("Bem vindo, "+ novaPf.Nome);
+
+
+//     //Aula calcular imposto:
+    
+//         //pessoa fisica:
+//         float ImpostoPagarPF = novaPf.CalcularImposto(novaPf.rendimentos);
+//         Console.WriteLine(ImpostoPagarPF.ToString("C"));
+
+//         //pessoa juridica
+//         float ImpostoPagarPJ = novaPJ.CalcularImposto(novaPJ.rendimentos);
+//         Console.WriteLine(ImpostoPagarPJ);
+
+//     //
+
+    
+//     //Aula validar data de nascimento:
+//         Console.WriteLine(novaPf.ValidarDataNasc("01/jan/2000"));
+//     //
+
+//     //Aula mostrar o endereço total:
+//         //@ formatar o formulario como quiser: 
+//         Console.WriteLine(@$"
         
-        Nome: {novaPf.Nome}
-        Endereço: {novoEndPf.logradouro }, NUm: {novoEndPf.numero}
-        Maior de Idade: {novaPf.ValidarDataNasc(novaPf.DataNasc)}        
-        ");
-        
+//         Nome: {novaPf.Nome}
+//         Endereço: {novoEndPf.logradouro }, NUm: {novoEndPf.numero}
+//         Maior de Idade: {novaPf.ValidarDataNasc(novaPf.DataNasc)}        
+//         ");
+//     //    
 
+
+    
